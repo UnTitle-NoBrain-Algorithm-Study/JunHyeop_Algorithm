0b1ed2e7369dff9c54fc5f4e079fb7d8b15868c8
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-06 12:10:18 +0900

    언타이틀 알고리즘 스터디 1주차 Level 3 숫자 문자열과 영단어

---
+++ 1주차/숫자문자열과영단어.py
@@ -0,0 +1,8 @@
+import re
+
+def solution(s):
+    dic={"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
+
+    for x,y in dic.items():
+        s=re.sub(x,str(y),s)
+    return int(s)
