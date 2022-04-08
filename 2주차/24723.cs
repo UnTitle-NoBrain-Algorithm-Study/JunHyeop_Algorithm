5172aba27c175871d6fe55eaeff36725f94a6d1e
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-08 12:32:20 +0900

    언타이틀 알고리즘 스터디 2주차 Level 1 녹색거탑

---
+++ 2주차/24723.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int num = int.Parse(input);
+            int result=1;
+
+            for (int cir = 0; cir < num; cir++)
+            {
+                result *= 2;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
