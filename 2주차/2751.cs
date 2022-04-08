da6ed8c3ade339baf08b61ba7809e17729389805
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-08 12:32:31 +0900

    언타이틀 알고리즘 스터디 2주차 Level 2 수 정렬하기 2

---
+++ 2주차/2751.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] numbersarr = new int[number];
+
+            for (int index = 0; index < number; index++)
+            {
+                numbersarr[index] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbersarr);
+
+            StringBuilder sb = new StringBuilder(string.Join("\n", numbersarr));
+
+            Console.WriteLine(sb);
+        }
+    }
+}
