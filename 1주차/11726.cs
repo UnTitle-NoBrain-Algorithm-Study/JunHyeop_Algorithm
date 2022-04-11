d03c4fa977492197f79519efe9ad1835d495d3dd
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-11 16:21:46 +0900

    언타이틀 알고리즘 스터디 1주차 Level 5 2×n 타일링

---
+++ 1주차/11726.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            const int MAXINDEX = 1000;
+            int[] square = new int[MAXINDEX];
+
+            square[0] = 1;
+            square[1] = 2;
+
+            for (int index = 2; index < MAXINDEX; index++)
+            {
+                square[index] = (square[index - 1] + square[index - 2]) % 10007;
+            }
+
+            Console.WriteLine(square[input - 1]);
+        }
+    }
+}
