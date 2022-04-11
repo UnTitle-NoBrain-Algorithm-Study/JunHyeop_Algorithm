10020683ffc11eec20c590192ca5b70db55cc490
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-11 16:21:59 +0900

    언타이틀 알고리즘 스터디 1주차 Level 4 영화감독 숌

---
+++ 1주차/1436.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] _666 = new int[10001];
+
+            int index = 1;
+            int num = 666;
+
+            while (index < 10000)
+            {
+                if (num.ToString().Contains("666"))
+                {
+                    _666[index] = num;
+                    index++;
+                }
+                num++;
+            }
+
+            Console.WriteLine(_666[input]);
+        }
+    }
+}
