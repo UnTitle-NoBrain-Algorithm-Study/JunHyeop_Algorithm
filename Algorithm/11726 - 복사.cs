dd2fa64e70670b2bcf2f86a1ac2daba197f7f6c3
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-13 01:03:55 +0900

    곧 포맷당할 파일 ㅠㅠ

---
+++ Algorithm/11726 - 복사.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            double rangeMin = Math.Pow(10, input - 2);
+            double rangeMax = Math.Pow(10, input - 1);
+
+
+            for (double num = rangeMin; num < rangeMax; num++)
+            {
+                if()
+            }
+        }
+    }
+}
