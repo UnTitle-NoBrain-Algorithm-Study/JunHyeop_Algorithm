62b4fab26a014698738dfb2c50afaf62467293c2
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-16 00:49:30 +0900

    언타이틀 알고리즘 스터디 3주차 Level 1 약수의 합

---
+++ 3주차/약수의 합.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithm
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            int answer = 0;
+
+            for (int num = 1; num <= Math.Sqrt(n); num++)
+            {
+                if (n % num == 0)
+                {
+                    if (n/num == num)
+                    {
+                        answer += num;
+                    }
+                    else
+                    {
+                        answer += num + (n/num);
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Solution solution = new Solution();
+
+            int result = solution.solution(input);
+
+            Console.WriteLine(result);
+        }
+    }
+}
