359b695dba1b2fe6df1594373786c41cb994710e
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-29 15:30:38 +0900

    언타이틀 알고리즘 스터디 5주차 Level 1 카드1

---
+++ 5주차/2161.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            List<int> arr = new List<int>();
+            int[] output = new int[input];
+            int index = 0;
+
+            for (int i = 1; i <= input; i++)
+            {
+                arr.Add(i);
+            }
+
+            for (int j = 0; j < input * 2 - 1; j += 2)
+            {
+                output[index] = arr[j];
+                index++;
+
+                try
+                {
+                    arr.Add(arr[j + 1]);
+                }
+                catch (Exception)
+                {
+                    break;
+                    throw;
+                }
+            }
+
+            foreach (int item in output)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
