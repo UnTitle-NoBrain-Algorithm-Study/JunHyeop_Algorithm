3f8e100baaf4b39b6d6878785b909b21b6cb3e18
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-04-29 17:42:12 +0900

    머지머지나머지

---
+++ Algorithm/2161 - 복사 - 복사.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] arr=new int[input];
+            long sum = 0;
+
+            for (int i = 0; i < input; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            for (int j = 0; j < input; j++)
+            {
+                sum += Math.Abs(j + 1 - arr[j]);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
