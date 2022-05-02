155f38a6d63ebcf35a78bfccd4b87a6a7b664298
이준협 <83260451+INFERNA3631@users.noreply.github.com>
2022-05-02 17:08:34 +0900

    언타이틀 알고리즘 스터디 6주차 Level 1 괄호(스택)

---
+++ 6주차/9012.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < input; i++)
+            {
+                string data = Console.ReadLine();
+                result.AppendLine(Inspect(data));
+            }
+
+            Console.WriteLine(result);
+        }
+
+        public static string Inspect(string data)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (char item in data)
+            {
+                switch (item)
+                {
+                    case '(':
+                        stack.Push(item);
+                        break;
+                    case ')':
+                        if (stack.Count > 0)
+                        {
+                            if (stack.Peek() == '(')
+                            {
+                                stack.Pop();
+                            }
+                            else
+                            {
+                                return "NO";
+
+                            }
+                        }
+                        else
+                        {
+                            return "NO";
+                        }
+                        break;
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+
+        }
+    }
+}
